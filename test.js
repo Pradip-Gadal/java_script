81a38d5bc1d67b4cb18d1d6a31fbac1e94350718
Pradip-Gadal <pradip.gadal.ds369@gmail.com>
2023-06-09 19:57:21 +0545

    update#4

--- test.js
+++ test.js
@@ -323,3 +323,425 @@ function TryGlobalVar() {
 decleareAnotherVar();
 TryGlobalVar();
 
+//# Local Scope and Function
+function localScope() {
+    var myValue = "infinite";
+    console.log(`my value is ${myValue}.`)
+}
+
+localScope();
+
+/* console.log(myValue) outside the function 
+will give error output beacuse it is local
+scope */
+
+ //# Global vs. Local scope in Functions
+ /* It's possiable to have local variable and
+ global variable with same name but Global 
+ veriable will take over outside the function*/
+
+ var outWare = "T-Shirt";
+
+ function myOutfit() {
+    var outWare = "Sweater";
+
+    return outWare;
+ }
+
+ console.log(`${myOutfit()} is local variable`)
+ console.log(`${outWare} is Global veriable`)
+
+ //# Return a Value from Return
+
+ function minusSeven(num) {
+    return num - 7;
+ }
+
+ console.log(minusSeven(10))
+
+ function timesFive(num) {
+    return num * 5;
+ }
+
+ console.log(timesFive(10))
+
+ //# Understanding Undefined Value Returned from a Function
+ 
+ var sum = 0
+ function sumThere() {
+    sum += 3;
+ }
+
+ sumThere()
+
+ var sum = 0
+ function sumFive() {
+    sum += 5;
+    return sum;
+ }
+ console.log(sumFive())
+
+ //## Assignment with a Returned Value
+
+ var charged = 0;
+
+ function change(num) {
+    return (num + 5)/3;
+ }
+
+ charged = change(10)
+ console.log(charged)
+
+ var processed = 0
+ function process(num) {
+    return (num+10)/3
+ }
+ processed = process(5)
+ console.log(processed)
+
+ var total = 0;
+ function charged(num) {
+    return (num+12)
+ }
+ total = charged(6)
+ console.log(total)
+
+ //#Stand in Line
+
+ function nextInLine(arr, item) {
+    arr.push(item)
+    return arr.shift()
+ }
+
+ var testArr = [1,2,3,4,5]
+ console.log(`Before: ${JSON.stringify(testArr)}`)
+ console.log(nextInLine(testArr, 6))
+ console.log(`After: ${JSON.stringify(testArr)}`)
+
+ arr = [1,2,3,4,5]
+ function PushShift(contain, item) {
+    contain.push(item)
+    return contain.shift()
+ }
+
+ console.log(`Before: ${JSON.stringify(arr)}`)
+ console.log(PushShift(arr, 6))
+ console.log(`After: ${JSON.stringify(arr)}`)
+
+
+ var Name = ["Ishita", "Ishak", "Pradip"];
+function LimitLength(init, item) {
+    init.push(item)
+    return init.shift()
+}
+
+ console.log(`Initial: ${JSON.stringify(Name)}`)
+ console.log(LimitLength(Name,"Pramila"))
+ console.log(`Final: ${JSON.stringify(Name)}`)
+
+ //# Boolean Value:
+ function welcomeToBooleans() {
+    return true;
+ }
+
+ //# Use Conditional Logic with If Statements:
+  
+function trueOrFalse(value) {
+    if (value) {
+        return "the value is true";
+    }
+    return "the value is false";
+}
+
+console.log(trueOrFalse(1))
+
+function TestInput(value){
+    if (value) {
+        return `input is ${value}`;
+    }
+    return "no input value is given.\n give input value"
+}
+
+console.log(TestInput("hero"))
+console.log(TestInput())
+
+//# Comparison with the equality operator
+
+function TestValue(value) {
+    if (value == 5) {
+        return "True";
+    }
+    return "False";
+}
+
+console.log(TestValue(5))
+console.log(TestValue("5"))
+/* for value == 5, then the value 5 or "5"
+returns True where === is strict operators
+so it will returns true for only 5 where as 
+false for "5" */
+
+function TestInput(value) {
+    if (value === 5) {
+        return "true";
+    }
+    return "false";
+}
+
+console.log(TestInput(5))
+console.log(TestInput("5"))
+
+//# Practice Comparing Different Values:
+
+function compareEquality(a,b) {
+    if (a == b) {
+        return "True";
+    }
+    return "False";
+}
+
+console.log(compareEquality(10,"10"))
+
+function compareEquality(a,b) {
+    if (a===b) {
+        return "true";
+    }
+    return "false";
+}
+
+console.log(compareEquality(10, "10"))
+
+//# Other Operator
+
+function TestLessOrEqual(val) {
+    if (val <= 12) {
+        return "less then 12";
+    }
+    if (val <= 24) {
+        return `12 < ${val} <= 25`;
+    }
+    if (val <= 100) {
+        return `24 < ${val} < 101`;
+    }
+    return "greater then 100";
+}
+
+result = TestLessOrEqual(55)
+console.log(result)
+
+function TestGreaterOrEquall(value) {
+    if (value > 100) {
+        return "greater then 100";
+    }
+    if (value > 50) {
+        return "Greater then 50";
+    }
+    if (value > 10) {
+        return "greater then 10";
+    }
+    return "less then 10";
+}
+
+value = TestGreaterOrEquall(55)
+console.log(value)
+
+//# Comparisons with the Logical And Operator:
+
+function testLogicalAnd(value) {
+    if (value >= 50 && value <= 100){
+        return "True";
+    }
+    return "False";
+}
+
+val = testLogicalAnd(33)
+console.log(val);
+
+//# Comparisons with the logical or Operator
+
+function testLogicalOr(val) {
+    if (val>10 || val<50) {
+        return "true";
+    }
+    return "False";
+}
+
+value = testLogicalOr(44);
+console.log(value);
+
+//# Else Statements
+
+function TestELseIF(value) {
+    if (value == 10) {
+        return "true";
+    } else {
+        return "false";
+    }
+}
+val = TestELseIF(11);
+console.log(val);
+
+function testIfElseIf(value) {
+    if (value == 20) {
+        return "true";
+    } else if (value < 20) {
+        return "smaller!";
+    } else if (value > 100) {
+        return "Greater then 100";
+    } else {
+        return "greater";
+    }
+}
+val = testIfElseIf(44);
+console.log(val);
+
+//#Golf Code
+Name = ["FC", "Barsa", "Chalse", "Eagle", "Welcome", "Jumants", "Go Home"]
+function golfScore(strokes, par) {
+    if (strokes == 1) {
+        return Name[0];
+    } else if (strokes <= par-2) {
+        return Name[1];
+    } else if (strokes == par-1) {
+        return Name[2];
+    } else if (strokes == par) {
+        return Name[3];
+    } else if (strokes == par+1) {
+        return Name[4];
+    } else if (strokes == par+2) {
+        return Name[5];
+    } else if(strokes >= per + 3) {
+        return Name[6];
+    }
+}
+team = golfScore(5, 4);
+console.log(team)
+
+//#Switch Statement
+
+function UseOfSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gama";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    
+    return answer;
+}
+
+console.log(UseOfSwitch(1))
+
+
+function UseSwitch(value) {
+    var answer = "";
+    switch(value) {
+        case 1:
+            answer = "alpha";
+            break;
+    }
+    return answer;    
+}
+
+console.log(UseSwitch(1))
+
+function UseDefualtInSwitch(value){
+    var answer = "";
+    switch(value){
+        case "a":
+            answer = "Apple";
+            break;
+        case "b":
+            answer = "Ball";
+            break;
+        case "c":
+            answer = "Cat";
+            break;
+        default:
+            answer = "Unknown";
+            break;
+    }
+    return answer;
+}
+
+output = UseDefualtInSwitch("z")
+console.log(output)
+
+function sequencialSize(val) {
+    var message = "";
+   
+    switch(val){
+    case 1:
+    case 2:
+    case 3:
+        message = "low";
+        break;
+    case 4:
+    case 5:
+    case 6:
+        message = "mid";
+        break;
+    case 7:
+    case 8:
+    case 9:
+        message = "high";
+        break;
+    default:
+        message = "Not Recoznized"
+   } 
+   return message;
+}
+
+output = sequencialSize(10);
+console.log(output);
+
+//Returning Boolean Values from Functions:
+ 
+function returningExpression(a, b) {
+    return a < b;
+}
+output = returningExpression(10, 20);
+console.log(output)
+
+ //counting card
+ var count = 0;
+ function cc(card) {
+    switch(card){
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break
+        case 10:
+        case "J":
+        case "K":
+        case "Q":
+        case "A":
+            count--;   
+    }
+    var holdbet = "Hold"
+    if (count > 2){
+        holdbet = "bet";
+    }
+
+    return `${count} ${holdbet}`;
+
+ }
+
+ cc(2); cc(2); cc(2); cc(10)
+
+ console.log(cc(2))
+
+ //# java script object:
+
